814a26f45809f094589e545bb5f721ef76a794d9
Jim Dervay <jamesdervay@gmail.com>
2018-12-08 08:56:16 -0500

    first commit

---
+++ ADF5355 Interface/Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace ADF5355_Interface
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Button_1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(98, 13);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(1);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(67, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // Button_1
+            // 
+            this.Button_1.Location = new System.Drawing.Point(20, 11);
+            this.Button_1.Margin = new System.Windows.Forms.Padding(1);
+            this.Button_1.Name = "Button_1";
+            this.Button_1.Size = new System.Drawing.Size(74, 20);
+            this.Button_1.TabIndex = 1;
+            this.Button_1.Text = "Connect";
+            this.Button_1.UseVisualStyleBackColor = true;
+            this.Button_1.Click += new System.EventHandler(this.connect_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(122, 106);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(1);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(138, 74);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(20, 53);
+            this.button1.Margin = new System.Windows.Forms.Padding(1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(64, 35);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Update Registers";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(20, 99);
+            this.button4.Margin = new System.Windows.Forms.Padding(1);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(76, 35);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Initialization Sequence";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(20, 143);
+            this.button5.Margin = new System.Windows.Forms.Padding(1);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(76, 35);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Frequency Update";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.button7);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(20, 198);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(1);
+            this.groupBox1.Size = new System.Drawing.Size(317, 117);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Frequency Hopping";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(241, 67);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(1);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(60, 20);
+            this.textBox3.TabIndex = 12;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(225, 22);
+            this.button7.Margin = new System.Windows.Forms.Padding(1);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(76, 35);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "Set # Of Hop Cycles";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(136, 67);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(1);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(60, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(120, 22);
+            this.button6.Margin = new System.Windows.Forms.Padding(1);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(76, 35);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Set Hop Period (ms)";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(19, 67);
+            this.button3.Margin = new System.Windows.Forms.Padding(1);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(76, 35);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Start Hop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(19, 22);
+            this.button2.Margin = new System.Windows.Forms.Padding(1);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(76, 35);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Update Hop Registers";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox5);
+            this.groupBox2.Controls.Add(this.button10);
+            this.groupBox2.Controls.Add(this.button9);
+            this.groupBox2.Controls.Add(this.button8);
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Location = new System.Drawing.Point(20, 319);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(317, 84);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "External Sweep";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(241, 54);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(1);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(60, 20);
+            this.textBox5.TabIndex = 15;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(225, 17);
+            this.button10.Margin = new System.Windows.Forms.Padding(1);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(76, 35);
+            this.button10.TabIndex = 14;
+            this.button10.Text = "Set Minimum INT";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(19, 26);
+            this.button9.Margin = new System.Windows.Forms.Padding(1);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(76, 35);
+            this.button9.TabIndex = 13;
+            this.button9.Text = "Start Sweep";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(120, 17);
+            this.button8.Margin = new System.Windows.Forms.Padding(1);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(88, 35);
+            this.button8.TabIndex = 12;
+            this.button8.Text = "Set Sweep Duration (ms)";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(136, 54);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(1);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(60, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(122, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Status";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 448);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Button_1);
+            this.Controls.Add(this.comboBox1);
+            this.Margin = new System.Windows.Forms.Padding(1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button Button_1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ADF5355 Interface/Form1.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+using System.IO;
+
+
+namespace ADF5355_Interface
+{
+    public partial class Form1 : Form
+    {
+        bool isConnected = false;
+        String[] ports;
+        SerialPort port;
+        String[] registers;
+        String[] registersHop;
+        int milliseconds = 50;
+        string msDelay;
+        string hopCycles;
+        string txtRecieved;
+        string txtStop = "STPH";
+        string extTime;
+        string extIntMin;
+
+        public Form1()
+        {
+            InitializeComponent();
+            disableControls();
+            getAvailableComPorts();
+
+            foreach(string port in ports)
+            {
+                comboBox1.Items.Add(port);
+                Console.WriteLine(port);
+                if (ports[0] != null)
+                {
+                    comboBox1.SelectedItem = ports[0];
+                }
+            }
+        }
+        private void connect_Click(object sender, EventArgs e)
+        {
+            if (!isConnected)
+            {
+                connectToArduino();
+            }
+            else
+            {
+                disconnectFromArduino();
+            }
+        }
+
+        void getAvailableComPorts()
+        {
+            ports = SerialPort.GetPortNames();
+        }
+
+        private void connectToArduino()
+        {
+            isConnected = true;
+            string selectedPort = comboBox1.GetItemText(comboBox1.SelectedItem);
+            port = new SerialPort(selectedPort, 9600, Parity.None, 8, StopBits.One);
+            port.Open();
+            port.Write("#STAR\n");
+            Button_1.Text = "Disconnect";
+            enableControls();
+            textBox1.Text = "Connected";
+        }
+
+        private void disconnectFromArduino()
+        {
+            isConnected = false;
+            port.Write("#STOP\n");
+            port.Close();
+            Button_1.Text = "Connect";
+            disableControls();
+            resetDefaults();
+        }
+
+        private void enableControls()
+        {
+            button1.Enabled = true;
+            Button_1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = true;
+            button7.Enabled = true;
+            button8.Enabled = true;
+            button9.Enabled = true;
+            button10.Enabled = true;
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+            textBox3.Enabled = true;
+            textBox4.Enabled = true;
+            textBox5.Enabled = true;
+            groupBox1.Enabled = true;
+            groupBox2.Enabled = true;
+
+        }
+
+        private void disableControls()
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = false;
+            button7.Enabled = false;
+            button8.Enabled = false;
+            button9.Enabled = false;
+            button10.Enabled = false;
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            textBox3.Enabled = false;
+            textBox4.Enabled = false;
+            textBox5.Enabled = false;
+            groupBox1.Enabled = false;
+            groupBox2.Enabled = false;
+        }
+
+        private void resetDefaults()
+        {
+            textBox1.Text = "";
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+                if (File.Exists(@"C:\Users\james\Desktop\ADFx35x_register_values.txt"))
+                {
+                    textBox1.Text = "Updating registers...";
+                    registers = System.IO.File.ReadAllLines(@"C:\Users\james\Desktop\ADFx35x_register_values.txt");
+                    port.Write("#REGS00" + registers[12] + "#\n");
+                    Thread.Sleep(milliseconds);
+                    port.Write("#REGS01" + registers[11] + "#\n");
+                    Thread.Sleep(milliseconds);
+                    port.Write("#REGS02" + registers[10] + "#\n");
+                    Thread.Sleep(milliseconds);
+                    port.Write("#REGS03" + registers[9] + "#\n");
+                    Thread.Sleep(milliseconds);
+                    port.Write("#REGS04" + registers[8] + "#\n");
+                    Thread.Sleep(milliseconds);
+                    port.Write("#REGS05" + registers[7] + "#\n");
+                    Thread.Sleep(milliseconds);
+                    port.Write("#REGS06" + registers[6] + "#\n");
+                    Thread.Sleep(milliseconds);
+                    port.Write("#REGS07" + registers[5] + "#\n");
+                    Thread.Sleep(milliseconds);
+                    port.Write("#REGS08" + registers[4] + "#\n");
+                    Thread.Sleep(milliseconds);
+                    port.Write("#REGS09" + registers[3] + "#\n");
+                    Thread.Sleep(milliseconds);
+                    port.Write("#REGS10" + registers[2] + "#\n");
+                    Thread.Sleep(milliseconds);
+                    port.Write("#REGS11" + registers[1] + "#\n");
+                    Thread.Sleep(milliseconds);
+                    port.Write("#REGS12" + registers[0] + "#\n");
+                    textBox1.Text = "Registers updated";
+                }
+                else
+                {
+                    textBox1.Text = "File not found";
+                }
+            }
+        }
+        private void button4_Click(object sender, EventArgs e) //Send initialize command
+        {
+            if (isConnected)
+            {
+                port.Write("#INIT\n");
+                textBox1.Text = "Initialization sequence written";
+            }
+        }
+        private void button5_Click(object sender, EventArgs e) // Send frequency update command
+        {
+            if (isConnected)
+            {
+                port.Write("#FREQ\n");
+                textBox1.Text = "Frequency update sequence written";
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+                if (File.Exists(@"C:\Users\james\Desktop\ADFx35x_register_values_hop.txt"))
+                {
+                    registersHop = System.IO.File.ReadAllLines(@"C:\Users\james\Desktop\ADFx35x_register_values_hop.txt");
+                    port.Write("#HOPS00" + registersHop[12] + "#\n");
+                    Thread.Sleep(milliseconds);
+                    port.Write("#HOPS01" + registersHop[11] + "#\n");
+                    Thread.Sleep(milliseconds);
+                    port.Write("#HOPS02" + registersHop[10] + "#\n");
+                    Thread.Sleep(milliseconds);
+                    port.Write("#HOPS03" + registersHop[9] + "#\n");
+                    Thread.Sleep(milliseconds);
+                    port.Write("#HOPS04" + registersHop[8] + "#\n");
+                    Thread.Sleep(milliseconds);
+                    port.Write("#HOPS05" + registersHop[7] + "#\n");
+                    Thread.Sleep(milliseconds);
+                    port.Write("#HOPS06" + registersHop[6] + "#\n");
+                    Thread.Sleep(milliseconds);
+                    port.Write("#HOPS07" + registersHop[5] + "#\n");
+                    Thread.Sleep(milliseconds);
+                    port.Write("#HOPS08" + registersHop[4] + "#\n");
+                    Thread.Sleep(milliseconds);
+                    port.Write("#HOPS09" + registersHop[3] + "#\n");
+                    Thread.Sleep(milliseconds);
+                    port.Write("#HOPS10" + registersHop[2] + "#\n");
+                    Thread.Sleep(milliseconds);
+                    port.Write("#HOPS11" + registersHop[1] + "#\n");
+                    Thread.Sleep(milliseconds);
+                    port.Write("#HOPS12" + registersHop[0] + "#\n");
+                    textBox1.Text = "Hop registers updated";
+                }
+                else
+                {
+                    textBox1.Text = "File not found";
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e) // Send hop period value
+        {
+            if (isConnected)
+            {
+                msDelay = textBox2.Text;
+                port.Write("#HOPD" + msDelay + "#\n");
+                textBox1.Text = "Hop period (ms) set";
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e) // Send hop cycles 
+        {
+            if (isConnected)
+            {
+                hopCycles = textBox3.Text;
+                port.Write("#HOPC" + hopCycles + "#\n");
+                textBox1.Text = "Number of hop cycles set";
+            }
+        }
+
+        private void button3_Click_1(object sender, EventArgs e) // Begin bopping
+        {
+            if (isConnected)
+            {
+                textBox1.Text = "Hopping...";
+                port.Write("#HOPB\n");
+                txtRecieved = port.ReadExisting();
+                while (!txtRecieved.Equals(txtStop))
+                {
+                    txtRecieved = port.ReadExisting();
+                }
+                textBox1.Text = "Hopping finished";
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e) // Set ext sweep time
+        {
+            if (isConnected)
+            {
+                extTime = textBox4.Text;
+                port.Write("#EXTT" + extTime + "#\n");
+                textBox1.Text = "Sweep time duration set";
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e) // Start ext sweep
+        {
+            if (isConnected)
+            {
+                textBox1.Text = "External sweeping...";
+                port.Write("#EXTS\n");
+                txtRecieved = port.ReadExisting();
+                while (!txtRecieved.Equals(txtStop))             {
+                    txtRecieved = port.ReadExisting();
+                }
+                textBox1.Text = "External sweeping finished";
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e) //Set minimum integer of external sweep
+        {
+            if (isConnected)
+            {
+                extIntMin = textBox5.Text;
+                port.Write("#EXTI" + extIntMin + "#\n");
+                textBox1.Text = "Minimum integer of external sweep set";
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
